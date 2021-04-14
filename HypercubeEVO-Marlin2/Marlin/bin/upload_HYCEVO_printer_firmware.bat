58df3e4c478b3f0777e7b8905235de14a1976c3f
hgabor47 <hgabor47@gmail.com>
2021-04-14 19:15:24 +0200

    Precompiled firmwares and upload BAT implemented

---
+++ HypercubeEVO-Marlin2/Marlin/bin/upload_HYCEVO_printer_firmware.bat
@@ -0,0 +1,2 @@
+C:\Users\Gabi\.platformio\packages\tool-avrdude\avrdude.exe -Cavrdude.conf -v -patmega2560 -cwiring -PCOM4 -b115200 -D -Uflash:w:"C:\munka\HypercubeEVO-with-dual-extruder-controlled-fan-heated-bed-Marlin-2.0\HypercubeEVO-Marlin2\Marlin\bin\firmware.hex":i
+
